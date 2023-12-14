9f54cf3c5b366bf7ac4092bf451f7ccac160d52b
Jack <jack20220723@gmail.com>
2023-12-14 10:30:41 +0800

    Fix wrong URL keys as formatLighthouseReport input & Run lighthouse PR'ed branch

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -85,7 +85,7 @@ const isBranchDeploy =
 // Used to debug production build issues faster
 const isBuildFast = !!process.env.BUILD_FAST;
 
-const baseUrl = process.env.BASE_URL ?? '/docusaurus';
+const baseUrl = process.env.BASE_URL ?? '/docusaurus/';
 
 // Special deployment for staging locales until they get enough translations
 // https://app.netlify.com/sites/docusaurus-i18n-staging
